bd1e965731995b7de93c6fdee53b82f7142a1bfe
Logan <chenavier.logan@gmail.com>
2020-11-08 18:28:00 +0100

    brand setting

--- src/main/java/project/brand/Brand.java
+++ src/main/java/project/brand/Brand.java
@@ -15,4 +15,32 @@ public class Brand {
     private int id;
     private String name;
 
+    public int getId() {
+        return id;
+    }
+
+    public Brand(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Brand{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
